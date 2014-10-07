20ff6bf87a46b661b1e66204f8e9c3aa824cc43a
Prasanth <prashanth702@gmail.com>
2014-10-07 21:18:25 +0530

    Layout Init

--- sections/index/index_controller.js
+++ sections/index/index_controller.js
@@ -1,7 +1,9 @@
 angular.module('codehardApp')
     .controller('IndexController', ['IndexService',
         function(IndexService) {
+        	  var self = this;
 
-
+        	  self.headerCollapse = false;
+              self.navList = IndexService.getNavList();
         }
     ]);
